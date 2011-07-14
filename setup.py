9f0224c354f1b29cc1a1ceea41c9ff7bab259a0a
ip2k <github@seanp2k.endjunk.com>
2011-07-13 23:17:48 -0400

    Added main definition so it should work on Windows.  Turned project into a package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pycliweather',
+    version='0.3',
+    packages=['pycliweather',],
+    license='Attribution-NonCommercial-ShareAlike 3.0 Unported',
+    long_description=open('README.txt').read(),
+)
